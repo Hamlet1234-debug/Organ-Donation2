3e2c552bbca013af07741ad3c7ae76369c43f2cd
Hamlet1234-debug <20242073010@nwu.ac.bd>
2026-09-13 22:05:10 +0600

    Organ Donation Management System – UI Design of the LoginFirstPage and Home page, created using Java Swing in NetBeans.
    LoginFirstPage: This is the first page displayed when the user enters the application. It provides options to access the different functionalities of the system.
    
    Home: This page is designed for the administrator and contains the main admin functionalities for managing the application.

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project;
+import java.sql.*;
+/**
+ *
+ * @author monda
+ */
+public class ConnectionProvider {
+    
+    public static Connection getCon()
+    {
+        try{
+        //Class.forName("com.mysql.jdbc.driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/odms","root","");
+        return con;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
---
+++ src/lifesaver/LifeSaver.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lifesaver;
+
+//import loginDonor;
+
+/**
+ *
+ * @author monda
+ */
+public class LifeSaver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // new LoginFirstPage().setVisible(true);
+    }
+    
+}
